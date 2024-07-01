a2972d61f7c7cbcb54a3ce450c317b9049677399
Louis Coleman <coleloui18@gmail.com>
2024-06-30 21:01:45 -0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,36 @@
 """Scrape fed rate"""
 
-url = "https://www.federalreserve.gov/releases/h15/"
-print("this is testing")
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+URL = "https://www.federalreserve.gov/releases/h15/"
+
+result = requests.get(URL, timeout=30)
+doc = BeautifulSoup(result.text, "html.parser")
+
+tr = doc.find_all("tr")
+
+# with open('data', 'w', newline='') as file:
+#     writer = csv.writer(file)
+
+th = []
+
+for item in tr[0]:
+    if item == "\n":
+        continue
+    else:
+        th.append(item.text)
+
+for i, val in enumerate(tr):
+    if i == 0:
+        continue
+
+    for item in val:
+        row.append(item.text)
+
+# def rowBuilder(arr):
+#     for item in arr:
+
+
+# print(th)
--- dockerfile
+++ dockerfile
@@ -1,4 +1,12 @@
 FROM python
-WORKDIR /app
-COPY . /app
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+
+RUN pip install --no-cache-dir --upgrade pip \
+    && pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
 CMD ["python", "app.py"]
