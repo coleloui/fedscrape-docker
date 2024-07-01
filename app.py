d8ccce9a9c1d5c5b2bbfe39d8b2a1e9b2323599d
Louis Coleman <coleloui18@gmail.com>
2024-07-01 13:55:34 -0700

    build csv

--- app.py
+++ app.py
@@ -1,36 +1,60 @@
 """Scrape fed rate"""
 
-import requests
 import csv
+import unicodedata
 from bs4 import BeautifulSoup
+import requests
 
+# Fed URL
 URL = "https://www.federalreserve.gov/releases/h15/"
 
+
+def table_constructor(data):
+    """Function to take html data and convert it into a
+    list of lists with the intention of returning these
+    to be made into a csv"""
+
+    # Function variables
+    table_data = []
+    table_row = []
+
+    # Loop through list of list to obtain and sanitize each value
+    # so that if it is a number it is a float and a string is a string
+    for val in data:
+        for item in val:
+            if item == "\n":
+                continue
+            else:
+                # Sanitize value to get the base text
+                text_string = unicodedata.normalize("NFKD", item.text).rstrip()
+                # check to see if the value is a float
+                try:
+                    float(text_string)
+                    # if so convert the value and add to list
+                    table_row.append(float(text_string))
+                # if not append the string
+                except ValueError:
+                    table_row.append(text_string)
+        # append the list that is each row of the table to the overall table data
+        table_data.append(table_row)
+        # clear the row to a clean list for the next list
+        table_row = []
+    # Return list of lists that contain the sanitized values
+    return table_data
+
+
+# request the html from the website
 result = requests.get(URL, timeout=30)
+# use Beautiful Soup to parse the HTML
 doc = BeautifulSoup(result.text, "html.parser")
 
-tr = doc.find_all("tr")
+# find all of the table rows in the parsed HTML document
+html_data = doc.find_all("tr")
 
-# with open('data', 'w', newline='') as file:
-#     writer = csv.writer(file)
+# call our custom function to constuct our table data
+table_data_array = table_constructor(html_data)
 
-th = []
-
-for item in tr[0]:
-    if item == "\n":
-        continue
-    else:
-        th.append(item.text)
-
-for i, val in enumerate(tr):
-    if i == 0:
-        continue
-
-    for item in val:
-        row.append(item.text)
-
-# def rowBuilder(arr):
-#     for item in arr:
-
-
-# print(th)
+# create a csv with the constructed data from our function
+with open("data.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerows(table_data_array)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if text_string.isnumeric():
+                    #     table_row.append(int(text_string))
+                    # else:
+                    #     table_row.append(int(text_string))
