36f60013268a5aaf2e213d5de4c91bf2d63c6bdf
Louis Coleman <coleloui18@gmail.com>
2024-07-21 09:09:27 -0700

    update URL and test file download

--- request_data.py
+++ request_data.py
@@ -2,12 +2,13 @@
 
 import requests
 
-FFER_URL = (
-    "https://www.federalreserve.gov/datadownload/Download.aspx?rel=H15&series=c5025f4bbbed155"
-    "a6f17c587772ed69e&filetype=csv&label=include&layout=seriescolumn&from=01/01/2000&to=07/17/2024"
+FED_FUND_EFFECTIVE_RATE_URL = (
+    "https://www.federalreserve.gov/datadownload/Output.aspx?rel=H15&series=c5025f4bbbed155a6f17c58"
+    "7772ed69e&lastobs=&from=01/01/2000&to=07/17/2024&filetype="
+    "csv&label=include&layout=seriescolumn"
 )
 
-response = requests.get(FFER_URL, timeout=30)
+response = requests.get(FED_FUND_EFFECTIVE_RATE_URL, timeout=30)
 
-with open("data/ffer.csv", mode="wb") as file:
+with open("data/fed_fund_eff_rate.csv", mode="wb") as file:
     file.write(response.content)
