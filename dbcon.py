71c28aaa2768bafcf539cd69724e15af61f6c9e8
Louis Coleman <coleloui18@gmail.com>
2024-08-04 15:02:12 -0700

    testing queries

--- dbcon.py
+++ dbcon.py
@@ -9,11 +9,63 @@ connection = psycopg2.connect(
 
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+cursor.execute(
+    """CREATE TABLE federal_funds (
+    id INT PRIMARY KEY UNIQUE NOT NULL,
+    date DATE UNIQUE NOT NULL,
+    rate VARCHAR(50) NOT NULL);
+    """
+)
 
-cursor.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-cursor.execute("SELECT * FROM test;")
-print(cursor.fetchone())
+cursor.execute(
+    """
+    CREATE TABLE scrape (           
+    id INT PRIMARY KEY UNIQUE NOT NULL,
+    date DATE UNIQUE NOT NULL,
+    "Federal funds" VARCHAR(50) NOT NULL,
+    "Commercial Paper - Nonfinancial - 1 Month" VARCHAR(50) NOT NULL,
+    "Commercial Paper - Nonfinancial - 2 Month" VARCHAR(50) NOT NULL,
+    "Commercial Paper - Nonfinancial - 3 Month" VARCHAR(50) NOT NULL,
+    "Commercial Paper - Financial - 1 Month" VARCHAR(50) NOT NULL,
+    "Commercial Paper - Financial - 2 Month" VARCHAR(50) NOT NULL,
+    "Commercial Paper - Financial - 3 Month" VARCHAR(50) NOT NULL,
+    "Bank prime loan" VARCHAR(50) NOT NULL,
+    "Discount window primary credit" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury bills - 4 week" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury bills - 3 month" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury bills - 6 month" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury bills - 1 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 1 month" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 3 month" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 6 month" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 1 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 2 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 3 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 5 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 7 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 10 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 20 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Nominal 9 - 30 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Inflation indexed - 5 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Inflation indexed - 7 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Inflation indexed - 10 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Inflation indexed - 20 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Tresury - Inflation indexed - 30 year" VARCHAR(50) NOT NULL,
+    "U.S. gov securities - Inflation-indexed long-term average" VARCHAR(50) NOT NULL);"""
+)
+
+print(
+    cursor.execute(
+        """SELECT
+                id,
+                date,
+                rate
+            FROM 
+                information_schema.columns
+            WHERE 
+                table_name = federal_funds;"""
+    )
+)
 
 cursor.close()
 connection.close()
