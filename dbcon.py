ca4092125622c3d207b20f920266bf3aac0e6b23
Louis Coleman <coleloui18@gmail.com>
2024-08-11 16:33:52 -0700

    add tables

--- dbcon.py
+++ dbcon.py
@@ -9,14 +9,6 @@ connection = psycopg2.connect(
 
 cursor = connection.cursor()
 
-# cursor.execute(
-#     """CREATE TABLE federal_funds (
-#     id SERIAL PRIMARY KEY UNIQUE,
-#     date DATE UNIQUE NOT NULL,
-#     rate VARCHAR(50) NOT NULL);
-#     """
-# )
-
 cursor.execute(
     """
     CREATE TABLE scrape (
@@ -52,6 +44,95 @@ cursor.execute(
     "U.S. gov - Maturities - Inflation indexed - 20 year" VARCHAR(50) NOT NULL,
     "U.S. gov - Maturities - Inflation indexed - 30 year" VARCHAR(50) NOT NULL,
     "U.S. gov - Inflation-indexed long-term average" VARCHAR(50) NOT NULL);"""
+)
+
+cursor.execute(
+    """
+    CREATE TABLE federal_funds (
+      id SERIAL PRIMARY KEY UNIQUE,
+      date DATE UNIQUE NOT NULL,
+      rate VARCHAR(50) NOT NULL);
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE commercial_paper_nonfinancial (
+        id SERIAL PRIMARY KEY UNIQUE,
+        date DATE UNIQUE NOT NULL,
+        one_month VARCHAR(50) NOT NULL,
+        two_month VARCHAR(50) NOT NULL,
+        three_month VARCHAR(50) NOT NULL);
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE commercial_paper_financial (
+        id SERIAL PRIMARY KEY UNIQUE,
+        date DATE UNIQUE NOT NULL,
+        one_month VARCHAR(50) NOT NULL,
+        two_month VARCHAR(50) NOT NULL,
+        three_month VARCHAR(50) NOT NULL);
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE bank_prime_loan (
+      id SERIAL PRIMARY KEY UNIQUE,
+      date DATE UNIQUE NOT NULL,
+      rate VARCHAR(50) NOT NULL);
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE discount_window_primary_credit (
+      id SERIAL PRIMARY KEY UNIQUE,
+      date DATE UNIQUE NOT NULL,
+      rate VARCHAR(50) NOT NULL);
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE us_gov_securities_tresury_bills (
+        id SERIAL PRIMARY KEY UNIQUE,
+        date DATE UNIQUE NOT NULL,
+        four_week VARCHAR(50) NOT NULL,
+        three_month VARCHAR(50) NOT NULL,
+        six_month VARCHAR(50) NOT NULL,
+        one_year VARCHAR(50) NOT NULL);
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE maturties_nominal_9 (
+        id SERIAL PRIMARY KEY UNIQUE,
+        date DATE UNIQUE NOT NULL,
+        one_month VARCHAR(50) NOT NULL,
+        three_month VARCHAR(50) NOT NULL,
+        six_month VARCHAR(50) NOT NULL,
+        one_year VARCHAR(50) NOT NULL,
+        two_year VARCHAR(50) NOT NULL,
+        three_year VARCHAR(50) NOT NULL,
+        five_year VARCHAR(50) NOT NULL,
+        seven_year VARCHAR(50) NOT NULL,
+        ten_year VARCHAR(50) NOT NULL,
+        twenty_year VARCHAR(50) NOT NULL,
+        thirty_year VARCHAR(50) NOT NULL);
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE constant_inflation_indexed (
+    id SERIAL PRIMARY KEY UNIQUE,
+    date DATE UNIQUE NOT NULL,
+    long_term_average VARCHAR(50) NOT NULL);
+    """
 )
 
 connection.commit()
